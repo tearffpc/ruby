1e12e70353dfa26bcb9daa47059f26700ac6ad70
nabuhandbz <90187970+nabuhandbz@users.noreply.github.com>
2021-09-11 12:00:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Amalser working perfectly 💘')
+            chalk.green.bold('Ruby working perfectly 💘')
         );
     });
     
@@ -245,7 +245,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, 'AMALSERBOT_🐱_[error] ' +
+                                await conn.sendMessage(conn.user.jid, 'RUBY_🐱_[error] ' +
                                     '\n\n*👻 ' + error + '*\n'
                                     , MessageType.text);
                             }
